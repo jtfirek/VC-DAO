24f6555ce9fda4202db69d10f1b455bca76d5bee
jtfirek <106350168+jtfirek@users.noreply.github.com>
2022-10-12 11:42:23 -0400

    minted nft

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ const App = () => {
   if (hasClaimedNFT) {
     return (
       <div className="member-page">
-        <h1>🍪DAO Member Page</h1>
+        <h1>Electronic Capital Member Page</h1>
         <p>Congratulations on being a member</p>
       </div>
     );
@@ -77,7 +77,7 @@ const App = () => {
   // Render mint nft screen.
   return (
     <div className="mint-nft">
-      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <h1>Mint your Electronic Capital Membership NFT</h1>
       <button
         disabled={isClaiming}
         onClick={mintNft}
